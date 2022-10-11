93e2fc89be93459431dfbb00bce17f46a4a661df
MiltonEU <milton.eustache@gmail.com>
2022-10-11 10:57:44 -0400

    add react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
-
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 function App() {
   return (
    <>
-   <h1>Hello World !</h1>
+   <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<Signin/>}/>
+        <Route path="/sign-up" element={<Signup/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        <Route path="/offers" element={<Offers/>}/>
+      </Routes>
+   </Router>
    </>
   );
 }
